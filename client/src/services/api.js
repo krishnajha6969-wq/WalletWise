49aec62e0b79bac413b9c95e4db034d239746cb7
Krishna Jha <krishnajha6969@gmail.com>
2026-04-15 12:35:06 +0530

    fix: connect frontend to live Render backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://walletwise-backend-mv0s.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
